f5a8b0407d862cfa0d066572bbd35115fe978f7d
Özgün Doğan <ozgundgn0@gmail.com>
2023-01-25 17:10:38 +0300

    step

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -55,15 +56,17 @@ class _LoginViewState extends State<LoginView> {
                     final userCredential = await FirebaseAuth.instance
                         .signInWithEmailAndPassword(
                             email: email, password: password);
-                    print(userCredential);
+                    devtools.log(userCredential.toString());
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil('/notes/', (_) => false);
                   } on FirebaseAuthException catch (e) {
-                    print(e.code);
+                    devtools.log(e.code);
                     final user = await FirebaseAuth.instance.currentUser;
-                    print(user);
+                    devtools.log(user.toString());
                     if (e.code == 'user-not-found') {
-                      print('user not found');
+                      devtools.log('user not found');
                     } else {
-                      print('something else happened.');
+                      devtools.log('something else happened.');
                     }
                   }
                 },
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -55,13 +56,14 @@ class _RegisterViewState extends State<RegisterView> {
                     final userCredential = await FirebaseAuth.instance
                         .createUserWithEmailAndPassword(
                             email: email, password: password);
-                    print(userCredential);
+                    devtools.log(userCredential.toString());
                   } on FirebaseAuthException catch (e) {
                     if (e.code == 'weak-password') {
-                      print(FirebaseAuth.instance.currentUser);
-                      print('Weak password.');
+                      devtools
+                          .log(FirebaseAuth.instance.currentUser.toString());
+                      devtools.log('Weak password.');
                     } else {
-                      print(e);
+                      devtools.log(e.message.toString());
                     }
                   }
                 },
