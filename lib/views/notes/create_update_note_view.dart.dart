4653c88c7077ebadd0bebe796068e214100935f1
Özgün Doğan <ozgundgn0@gmail.com>
2023-02-24 15:00:59 +0300

    step-20

--- lib/views/notes/create_update_note_view.dart.dart
+++ lib/views/notes/create_update_note_view.dart.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/utilities/dialogs/get_arguments.dart';
